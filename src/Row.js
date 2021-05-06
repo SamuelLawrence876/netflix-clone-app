38fbc6c32517f603c42d0b7f8f84289ad3b7cff1
Samuel Lawrence <55727214+SamuelLawrence876@users.noreply.github.com>
2021-05-06 09:12:40 +0100

    deployment and row trailer prep

--- src/Row.js
+++ src/Row.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 import "./Row.css";
 
 function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   const base_url = "http://image.tmdb.org/t/p/original";
 
@@ -16,6 +19,29 @@ function Row({ title, fetchURL, isLargeRow }) {
     }
     fetchData();
   }, [fetchURL]);
+
+  const opts = {
+    height: "390",
+    width: "99%",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
+
+  const handleClick = (movie) => {
+    // console.table(movie?.title)
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -30,6 +56,9 @@ function Row({ title, fetchURL, isLargeRow }) {
             alt={movie.name}
           />
         ))}
+      </div>
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
       </div>
     </div>
   );
